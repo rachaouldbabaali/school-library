7fa22f9cd40f33c6b5639dcbe0f34fb669248a64
Racha OULDBABAALI <rachaould@gmail.com>
2023-07-24 18:43:34 +0100

    Setup project and Add classes

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative 'person'
+
+class Teacher < Person
+  attr_reader :specialization
+
+  def initialize(name = 'Unknown', age = nil, specialization = nil, parrent_permission = true)
+    super(name, age, parrent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
