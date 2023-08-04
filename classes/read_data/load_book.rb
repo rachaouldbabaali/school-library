cc48eb36a5a69be820087544c07d89d3806bf8ed
Wael Elsafty <waelelsafty07@gmail.com>
2023-08-04 11:53:52 +0300

    Add: read books

--- classes/read_data/load_book.rb
+++ classes/read_data/load_book.rb
@@ -1,1 +1,13 @@
-def read_books(file_path); end
+def read_books(file_path)
+    data = []
+    return false unless File.exist?(file_path)
+    file = File.read(file_path)
+    JSON.parse(file).each do |book|
+        data << Book.new(book['title'], book['author'])
+    end
+    data
+end
+
+
+
+
